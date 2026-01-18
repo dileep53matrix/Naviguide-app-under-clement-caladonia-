eddf4532268d43c121e9723a0ca06b8c3fa065ac
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2026-01-18 23:57:24 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ import { getCardinalDirection } from "./utils/getCardinalDirection";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+
 export default function App() {
   const mapRef = useRef(null);
   const [segments, setSegments] = useState([]);
